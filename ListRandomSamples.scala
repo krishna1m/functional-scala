a2bbbe2ad32e9e34838abff4e8b6be632ca77a4d
Manmohan Krishna <krishna.m.zyw@gmail.com>
2023-05-08 10:55:32 +0530

    ListRandomSamples.scala added

---
+++ ListRandomSamples.scala
@@ -0,0 +1,20 @@
+@main def m() =
+  val l = List(1, 3, 5, 62, 2, 2, 90)
+  println(l.sample(3))
+
+extension [A](l: List[A])
+  def sample(num: Int): List[A] = {
+    lazy val length = l.length
+    def rand: A = {
+      val randomI = scala.util.Random.between(0, length)
+      l(randomI)
+    }
+
+    @scala.annotation.tailrec
+    def loop(i: Int, acc: List[A]): List[A] = {
+      if(i == num) acc
+      else loop(i + 1, rand +: acc)
+    }
+    if(l.isEmpty) Nil
+    else loop(0, Nil)
+  }
